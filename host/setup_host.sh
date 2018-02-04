783b14ad21dc3b1c860fbd46245af243ea38a823
root <root@lsdf-28-015.scc.kit.edu>
2018-02-04 12:11:43 +0100

    add fio to install

--- host/setup_host.sh
+++ host/setup_host.sh
@@ -18,9 +18,10 @@ subscription-manager repos --disable="*"
 subscription-manager repos --enable="rhel-7-server-rhv-4-mgmt-agent-rpms"
 subscription-manager repos --enable="rhel-7-server-rpms"
 subscription-manager repos --enable="rh-gluster-3-for-rhel-7-server-rpms"
+rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 # install gluster
-yum install glusterfs-server bash-completion
+yum install glusterfs-server fio
 
 # fix network
 echo "GATEWAYDEV=ovirtmgmt" > /etc/sysconfig/network
@@ -37,6 +38,7 @@ multipath -F
 systemctl restart multipathd.service
 
 # clean partition table
+# adapt to your hardware disks
 for i in {b,c,d,e} ; do
 dd if=/dev/zero of=/dev/sd$i bs=1M count=100
 done
